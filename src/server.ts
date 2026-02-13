5fda5aac2edb8affbf67248b70ce3481dd016b66
sanarajan <sanamol87@gmail.com>
2026-02-13 18:01:22 +0530

    update rto section to admin approval

--- src/server.ts
+++ src/server.ts
@@ -73,10 +73,10 @@ app.use(errorMiddleware);
 
 // const port = process.env.PORT || 8080;
 
-const PORT = Number(process.env.PORT) || 8080;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 
